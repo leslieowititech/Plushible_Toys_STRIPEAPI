195b3cc5ceba664be68894d53604d035a6eff0f5
Leslie Owiti <leslieowiti@Leslies-Air-2.attlocal.net>
2021-11-30 23:19:40 -0500

    figuring out components I need

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-left'>
+                <h1>Plushible <span>Shop</span></h1>
+            </div>
+
+        </div>
+    )
+}
+
+export default Header;
---
+++ frontend/src/components/PlushibleCard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlushibleCard = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default PlushibleCard;
