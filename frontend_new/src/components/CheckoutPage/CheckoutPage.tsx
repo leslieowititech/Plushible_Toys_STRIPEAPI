8faa6fbdeb809b00a5c2057ce1c1e3006faeb83c
Leslie Owiti <leslieowiti@Leslies-Air-2.attlocal.net>
2021-12-05 02:14:51 -0500

    adding checkout visual functionality

---
+++ frontend_new/src/components/CheckoutPage/CheckoutPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './CheckoutPage.css';
+
+const CheckoutPage: React.FC = () => {
+    return (
+        <div>
+            CheckoutPage
+        </div>
+    )
+}
+
+export default CheckoutPage
--- frontend_new/src/index.tsx
+++ frontend_new/src/index.tsx
@@ -3,9 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
+
+const stripePromise = loadStripe("pk_test_51K1EcQBN69YBp2gzBltlGaRyM0uSu4br3TKYAlXmzcd8QrlPPeHXnfIT08a91hEvdx2vkDGSfqfBJHpyQ3TLJvjU00q0l0XY8p");
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </React.StrictMode>,
   document.getElementById('root')
 );
